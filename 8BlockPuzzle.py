28cf4f588a174fe65d1efa5070d8f8d438068f40
ericye16 <computereric@live.ca>
2012-07-20 18:46:56 +0000

    Fixed errors-should work now!

--- 8BlockPuzzle.py
+++ 8BlockPuzzle.py
@@ -1,18 +1,18 @@
+#!/usr/bin/env python
 #let's try and implement that block puzzle.
 
 class blockPuzzle_3:
     '''Stores all the stuff. You know what I mean.'''
-    goalState = [[0, 1, 2],
-                 [3, 4, 5],
-                 [6, 7, 8]]
+
     actions = ['L', 'R', 'U', 'D']
     transitions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     currentState = [[0 for x in range(3)] for y in range(3)]
     currentBlank = [-1, -1]
 
-    def __init__(self, beginState):
+    def __init__(self, beginState, path = []):
         if self.check(beginState):
-            self.currentState = list(beginState)
+            self.currentState = [list(s) for s in beginState]
+            self.path = path
         else:
             raise ValueError, 'Invalid initial puzzle.'
 
@@ -31,7 +31,7 @@ class blockPuzzle_3:
 
     def move(self, move):
         '''Returns the state if you moved the blank space _move_'''
-        print self.currentBlank
+        #print self.currentBlank
 
         if not self.isValidMove(self.currentBlank, self.transitions, move):
             print 'invalid move'
@@ -40,7 +40,7 @@ class blockPuzzle_3:
         nonBlankPlace = [self.currentBlank[0]-self.transitions[move][0],
                          self.currentBlank[1]-self.transitions[move][1]]
         nonBlankVal = self.currentState[nonBlankPlace[0]][nonBlankPlace[1]]
-        print nonBlankVal
+        #print nonBlankVal
         #quick replace
         
         self.currentState[nonBlankPlace[0]][nonBlankPlace[1]] = 0
@@ -49,44 +49,48 @@ class blockPuzzle_3:
         self.currentBlank[0] -= self.transitions[move][0]
         self.currentBlank[1] -= self.transitions[move][1]
 
+        self.path.append(move)
+
     def show(self):
         for row in self.currentState:
             print row
+        print '======='
 
     def isValidMove(self, blankPlace, transitions, move):
         newY = blankPlace[0] - transitions[move][0]
         newX = blankPlace[1] - transitions[move][1]
         if newY < 0 or newY > 2 or \
-           newX < 0 or newY > 2:
+           newX < 0 or newX > 2:
             return False
         else:
             return True
 
     def moveAndGet(self, move):
         currentState = list(self.getState())
-        newState = blockPuzzle_3(currentState)
+        newState = blockPuzzle_3(currentState, self.path[:])
         newState.move(move)
         return newState
 
     def getState(self):
         return self.currentState
 
-    def expand(self):
+    def getPath(self):
+        return self.path
+
+    def getDepth(self):
+        return len(self.path)
+
+    def expand(self, explored = set()):
         a = []
         for move in range(4):
-            print move
+            #print move
             if self.isValidMove(self.currentBlank, self.transitions, move):
-                self.show()
+                #self.show()
                 newState = self.moveAndGet(move)
-                newState.show()
-                a.append(newState)
-
-        ###so the problem, I think, is that when you make a new
-                ###instance of the class, the old and new
-                ###currentStates point to the same thing (or the values
-                ###in each point to the same things,)
-                ###so some really weird things are happening.
-
+                if newState.getState not in explored:
+                    #newState.show()
+                    a.append(newState)
+                    #newState.show()
                 
         return a
 
@@ -94,21 +98,40 @@ class blockPuzzle_3:
 ########################
 
 
+
+goalState = [[0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8]]
+
+
 ########################
 def depthFirstSearch():
     from collections import deque
     initBlock = blockPuzzle_3([[7, 2, 4],
                                 [5, 0, 6],
-                                [8, 3, 1]])
+                                [8, 3, 1]],
+                              [])
     explored = set()
-    frontier = deque(initBlock.getState)
+    frontier = deque([initBlock])
+    depth = -1
+    
+    currentNode = frontier.popleft()
+    while currentNode.getState != goalState:
+        currentDepth = currentNode.getDepth()
+        if currentDepth != depth:
+            depth = currentDepth
+            print 'Depth = %i' % depth
+        frontier.extend(currentNode.expand(explored))
+        explored.add(currentNode.getState)
+        #print 'frontier = ', frontier
+        currentNode = frontier.popleft()
+    print currentNode.getPath()
     
     
 if __name__ == '__main__':
-    problem = blockPuzzle_3([[7, 2, 4],
-                             [5, 0, 6],
-                             [8, 3, 1]])
-    new = problem.expand()
-##    for i in new:
-##        i.show()
-    
+    initBlock = blockPuzzle_3([[7, 2, 4],
+                                [0, 5, 6],
+                                [8, 3, 1]],
+                              [])
+    depthFirstSearch()
+
