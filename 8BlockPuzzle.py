d5809bf36dfc6d2dad709a3e3ec5ebfff4986887
Eric Ye <computereric@live.ca>
2012-07-20 18:18:03 -0400

    Made it more space-efficient by factoring out the common methods.

--- 8BlockPuzzle.py
+++ 8BlockPuzzle.py
@@ -1,75 +1,22 @@
 #!/usr/bin/env python
 #let's try and implement that block puzzle.
 
+
+actions = ['L', 'R', 'U', 'D']
+transitions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
 class blockPuzzle_3:
     '''Stores all the stuff. You know what I mean.'''
 
-    actions = ['L', 'R', 'U', 'D']
-    transitions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     currentState = [[0 for x in range(3)] for y in range(3)]
     currentBlank = [-1, -1]
 
     def __init__(self, beginState, path = []):
-        if self.check(beginState):
+        if check(self, beginState):
             self.currentState = [list(s) for s in beginState]
             self.path = path
         else:
             raise ValueError, 'Invalid initial puzzle.'
-
-    def check(self, state):
-        a = []
-        for row in range(3):
-            for element in range(3):
-                if state[row][element] == 0:
-                    self.currentBlank = [row, element]
-                a.append(state[row][element])
-        a.sort()
-        if a == range(9):
-            return True
-        else:
-            return False
-
-    def move(self, move):
-        '''Returns the state if you moved the blank space _move_'''
-        #print self.currentBlank
-
-        if not self.isValidMove(self.currentBlank, self.transitions, move):
-            print 'invalid move'
-            return
-        
-        nonBlankPlace = [self.currentBlank[0]-self.transitions[move][0],
-                         self.currentBlank[1]-self.transitions[move][1]]
-        nonBlankVal = self.currentState[nonBlankPlace[0]][nonBlankPlace[1]]
-        #print nonBlankVal
-        #quick replace
-        
-        self.currentState[nonBlankPlace[0]][nonBlankPlace[1]] = 0
-        self.currentState[self.currentBlank[0]][self.currentBlank[1]] = \
-                                                                nonBlankVal
-        self.currentBlank[0] -= self.transitions[move][0]
-        self.currentBlank[1] -= self.transitions[move][1]
-
-        self.path.append(move)
-
-    def show(self):
-        for row in self.currentState:
-            print row
-        print '======='
-
-    def isValidMove(self, blankPlace, transitions, move):
-        newY = blankPlace[0] - transitions[move][0]
-        newX = blankPlace[1] - transitions[move][1]
-        if newY < 0 or newY > 2 or \
-           newX < 0 or newX > 2:
-            return False
-        else:
-            return True
-
-    def moveAndGet(self, move):
-        currentState = list(self.getState())
-        newState = blockPuzzle_3(currentState, self.path[:])
-        newState.move(move)
-        return newState
 
     def getState(self):
         return self.currentState
@@ -80,23 +27,74 @@ class blockPuzzle_3:
     def getDepth(self):
         return len(self.path)
 
-    def expand(self, explored = set()):
-        a = []
-        for move in range(4):
-            #print move
-            if self.isValidMove(self.currentBlank, self.transitions, move):
-                #self.show()
-                newState = self.moveAndGet(move)
-                if newState.getState not in explored:
-                    #newState.show()
-                    a.append(newState)
-                    #newState.show()
-                
-        return a
-
-
 ########################
 
+
+def move(self, move):
+    '''Returns the state if you moved the blank space _move_'''
+    #print self.currentBlank
+
+    if not isValidMove(self, self.currentBlank, transitions, move):
+        print 'invalid move'
+        return
+
+    nonBlankPlace = [self.currentBlank[0]-transitions[move][0],
+                     self.currentBlank[1]-transitions[move][1]]
+    nonBlankVal = self.currentState[nonBlankPlace[0]][nonBlankPlace[1]]
+    #print nonBlankVal
+    #quick replace
+
+    self.currentState[nonBlankPlace[0]][nonBlankPlace[1]] = 0
+    self.currentState[self.currentBlank[0]][self.currentBlank[1]] = \
+                                                            nonBlankVal
+    self.currentBlank[0] -= transitions[move][0]
+    self.currentBlank[1] -= transitions[move][1]
+
+    self.path.append(move)
+
+def moveAndGet(self, movePlace):
+    currentState = list(self.getState())
+    newState = blockPuzzle_3(currentState, self.path[:])
+    move(newState, movePlace)
+    return newState
+
+def check(puzzle, state):
+    a = []
+    for row in range(3):
+        for element in range(3):
+            if state[row][element] == 0:
+                puzzle.currentBlank = [row, element]
+            a.append(state[row][element])
+    a.sort()
+    if a == range(9):
+        return True
+    else:
+        return False
+
+def isValidMove(self, blankPlace, transitions, move):
+    newY = blankPlace[0] - transitions[move][0]
+    newX = blankPlace[1] - transitions[move][1]
+    if newY < 0 or newY > 2 or \
+       newX < 0 or newX > 2:
+        return False
+    else:
+        return True
+
+
+def expand(self, explored = set()):
+    a = []
+    for movePlace in range(4):
+        if isValidMove(self, self.currentBlank, transitions, movePlace):
+            newState = moveAndGet(self, movePlace)
+            if newState.getState() not in explored:
+                a.append(newState)
+    return a
+
+
+def show(self):
+    for row in self.currentState:
+        print row
+    print '======='
 
 
 goalState = [[0, 1, 2],
@@ -111,27 +109,38 @@ def depthFirstSearch():
                                 [5, 0, 6],
                                 [8, 3, 1]],
                               [])
-    explored = set()
+    explored = []
     frontier = deque([initBlock])
     depth = -1
-    
+
     currentNode = frontier.popleft()
-    while currentNode.getState != goalState:
+    while currentNode.getState() != goalState:
         currentDepth = currentNode.getDepth()
         if currentDepth != depth:
             depth = currentDepth
             print 'Depth = %i' % depth
-        frontier.extend(currentNode.expand(explored))
-        explored.add(currentNode.getState)
+            print 'Size of explored = %i' % len(explored)
+            print 'Size of frontier = %i' % len(frontier)
+        frontier.extend(expand(currentNode, explored))
+        explored.append(currentNode.getState())
         #print 'frontier = ', frontier
         currentNode = frontier.popleft()
-    print currentNode.getPath()
-    
-    
+    for movement in currentNode.getPath():
+        print actions[movement],
+    return currentNode.getPath()
+
+
 if __name__ == '__main__':
     initBlock = blockPuzzle_3([[7, 2, 4],
                                 [0, 5, 6],
                                 [8, 3, 1]],
                               [])
     depthFirstSearch()
+##    stuff = [initBlock.getState()]
+##    show(initBlock)
+##    b = expand(initBlock, stuff)
+##    for i in b:
+##        show(i)
+##        stuff.append(i.getState())
+
 
